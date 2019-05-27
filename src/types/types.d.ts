4b2c91a0ba296dce210914bfa5ea9a6ea2ef98ab
Isac <isac_asj91@hotmail.com>
2019-05-27 20:53:55 -0300

    add types to cucumber html

---
+++ src/types/types.d.ts
@@ -0,0 +1,21 @@
+declare module 'cucumber-html-reporter' {
+  interface Options {
+    theme: string,
+    jsonFile: string,
+    jsonDir?: string, 
+    output: string,
+    screenshotsDirectory?: string,
+    reportSuiteAsScenarios: boolean,
+    ignoreBadJsonFile?: boolean,
+    launchReport: boolean,
+    columnLayout?: number,
+    storeScreenshots?: boolean,
+    noInlineScreenshots?: boolean,
+    name?: string,
+    metadata?: {
+      [key: string]: string
+    },
+  }
+
+  export function generate(options: any, callback: () => NodeJS.Process): void
+}
